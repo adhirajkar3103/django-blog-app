de56cac944d718338e25a382b819f59b979521bb
adhirajkar3103 <adhirajkar3103@gmail.com>
2022-12-24 01:44:57 +0530

    CRUD functionality completed

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
-from .views import BlogListView,BlogView,CreateBlogView
+from .views import BlogListView,BlogView,CreateBlogView,UpdateBlogView,DeleteBlogView
 urlpatterns = [
+    path('post/<int:pk>/delete',DeleteBlogView.as_view(),name='post_delete'),
+    path('post/<int:pk>/edit/',UpdateBlogView.as_view(),name='post_edit'),
     path('post/new/',CreateBlogView.as_view(),name='post_new'),
     path('post/<int:pk>/',BlogView.as_view(),name='blog_detail'),
     path('',BlogListView.as_view(),name='all_blogs'),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class BlogListView(ListView):
@@ -13,3 +14,13 @@ class CreateBlogView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title','author','body']
+
+class UpdateBlogView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+class DeleteBlogView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('all_blogs')
